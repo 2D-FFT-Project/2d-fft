9908918a200f7c8b1ec41457cf65a463227ca54f
gyzavyalov@yandex-team.ru <gyzavyalov@yandex-team.ru>
2022-10-29 21:19:41 +0300

    GH-2: Added python venv

---
+++ dev_utils/sync.sh
@@ -0,0 +1,9 @@
+#Creating enviroment
+python3 -m venv venv
+
+# Starting enviroment
+source ./venv/bin/activate
+# To deactivate run 'deactivate' in shell
+
+# Syncing python packages
+pip3 install -r dev_utils/requirements.txt
