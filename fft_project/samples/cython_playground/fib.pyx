521bf13ae920a36449e818d91d3975d1e0922bd6
gyzavyalov <gyzavyalov@yandex-team.ru>
2023-02-16 21:25:36 +0300

    infra: cython working

---
+++ fft_project/samples/cython_playground/fib.pyx
@@ -0,0 +1,13 @@
+# distutils: language=c++
+
+import cython
+
+cpdef fib(n):
+  """
+  Print the Fibonacci series up to n.
+  
+  """
+  a, b = 0, 1
+  while b < n:
+    print(b, end=' ')
+    a, b = b, a + b
--- setup.py
+++ setup.py
@@ -1,6 +1,30 @@
+import os
+
 from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+ignore_dirs = [
+    './venv'
+]
+
+def find_pyx(path='.'):
+    cython_files = []
+    for root, dirs, filenames in os.walk(path):
+        ignore_current = False
+        for dir in ignore_dirs:
+            ignore_current |= root.startswith(dir)
+        if ignore_current:
+            continue
+        for fname in filenames:
+            if fname.endswith('.pyx'):
+                cython_files.append(os.path.join(root, fname))
+    print(f'Cythonizing: {cython_files}')
+    return cython_files
+
 
 setup(
     name = 'fft_project',
-    packages = find_packages()
+    packages = find_packages(),
+    ext_modules = cythonize(find_pyx(), language_level=3, annotate=True),
+    zip_safe=False
 )
