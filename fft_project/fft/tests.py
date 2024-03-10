cffa5a674ce19892e12e57f31e7b557f64c5f1c7
iamyeezuschrist <camrymanjr@me.com>
2024-03-10 10:28:58 +0100

    scipy comparisons added, tests modified for scipy

--- fft_project/fft/tests.py
+++ fft_project/fft/tests.py
@@ -1,6 +1,6 @@
 import logging
 import time
-
+import scipy
 import numpy as np
 
 import fft_project
@@ -19,14 +19,14 @@ class fft_tests:
     def fft_cpp_impl_test():
         result = random_matrix(4096, 4096 * 2)
         start = time.time()
-        expected = np.fft.fft2(result)
-        numpy_time = time.time() - start
-        logging.info(f'Numpy: {numpy_time}')
+        expected = scipy.fft.fft2(result)
+        scipy_time = time.time() - start
+        logging.info(f'Scipy: {scipy_time}')
         start = time.time()
-        result = fft_project.fft2d(result, return_copy=True, use_threads=False)
+        result = fft_project.fft2d(result, return_copy=False, use_threads=False)
         our_time = time.time() - start
         logging.info(f'Our: {our_time}')
-        logging.info(f'{our_time / numpy_time * 100:.2f}% of Numpy time!')
+        logging.info(f'{our_time / scipy_time * 100:.2f}% of Scipy time!')
         expected = abs(expected)
         result = abs(result)
         correctness = np.allclose(result, expected, atol=0.05)
@@ -36,22 +36,22 @@ class fft_tests:
     @base.testing.test
     def inverse_test():
         a = random_matrix(4096 * 2, 4096)
-        transformed = fft_project.fft2d(a, return_copy=True, use_threads=True)
+        transformed = fft_project.fft2d(a, return_copy=False, use_threads=True)
         start = time.time()
-        inversed_numpy = np.fft.ifft2(transformed)
-        numpy_time = time.time() - start
-        logging.info(f'Numpy: {numpy_time}')
+        inversed_scipy = scipy.fft.ifft2(transformed, workers = -1)
+        scipy_time = time.time() - start
+        logging.info(f'Scipy: {scipy_time}')
         inversed_our = transformed
         start = time.time()
         inversed_our = fft_project.fft2d(
-            inversed_our, return_copy=True, use_threads=False, inverse=True
+            inversed_our, return_copy=False, use_threads=True, inverse=True
         )
         our_time = time.time() - start
         logging.info(f'Our: {our_time}')
-        logging.info(f'{our_time / numpy_time * 100:.2f}% of Numpy time!')
-        inversed_numpy = abs(inversed_numpy)
+        logging.info(f'{our_time / scipy_time * 100:.2f}% of Scipy time!')
+        inversed_scipy = abs(inversed_scipy)
         inversed_our = abs(inversed_our)
-        correctness = np.allclose(inversed_our, inversed_numpy, atol=0.05)
+        correctness = np.allclose(inversed_our, inversed_scipy, atol=0.05)
         logging.info(f'Is the result correct: {correctness}')
         assert correctness
 
